e15817df93349306aad721ed0433b5cf57d61835
Mathew Allington <funkymallington@gmail.com>
2020-07-23 21:36:53 +0100

    First commit of many!

---
+++ src/main/java/Mallington/homesecurityapi/HomeSecurityApiApplication.java
@@ -0,0 +1,13 @@
+package Mallington.homesecurityapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeSecurityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeSecurityApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Mallington/homesecurityapi/HomeSecurityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Mallington.homesecurityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeSecurityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
