354f9b6e6f178fc9643a21252721d59f32e9ecc4
Mathew Allington <funkymallington@gmail.com>
2020-08-24 10:34:48 +0100

    Breaking changes

--- host-firmware/src/main.cpp
+++ host-firmware/src/main.cpp
@@ -1,27 +1,13 @@
 #define ARDUINOJSON_ENABLE_STD_STRING 0
 #define ARDUINOJSON_ENABLE_STD_STREAM 0
+
 #include "ArduinoJson.h"
 #include <SPI.h>
 #include <WiFiNINA.h>
+
 #include <network/WiFiUtils.h>
-
-
-
 #include "run_parameters.h"
 #include <network/RestAPI.h> 
-///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = WIFI_SSID;        // your network SSID (name)
-char pass[] = WIFI_PASS;    // your network password (use for WPA, or use as key for WEP)
-int keyIndex = 0;            // your network key Index number (needed only for WEP)
-
-
-// if you don't want to use DNS (and reduce your sketch size)
-// use the numeric IP instead of the name for the server:
-//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
-
-// Initialize the Ethernet client library
-// with the IP address and port of the server
-// that you want to connect to (port 80 is default for HTTP):
 
 WiFiClient client;
 RestAPI api(API_ADDRESS, API_PORT, client);
@@ -33,7 +19,7 @@ void setup() {
   }
 
   // check for the WiFi module:
-  int status = WiFiUtils.connectWiFI(ssid, pass, 10000);
+  int status = WiFiUtils.connectWiFI(WIFI_SSID, WIFI_PASS, 10000);
   if(status == WL_CONNECTED){
   Serial.println("Connected to wifi");
   WiFiUtils.printWiFiStatus();
