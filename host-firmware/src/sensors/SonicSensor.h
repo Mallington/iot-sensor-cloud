50d439765f491e458c2661e6e8f3182906ba0a87
Mathew Allington <funkymallington@gmail.com>
2020-08-24 21:26:13 +0100

    Testing sonic

--- host-firmware/src/sensors/SonicSensor.h
+++ host-firmware/src/sensors/SonicSensor.h
@@ -9,6 +9,11 @@ public:
     virtual bool setup(DynamicJsonDocument* deviceJSON);
     virtual DynamicJsonDocument* getData();
     virtual bool updateState(DynamicJsonDocument* deviceJSON);
+private:
+    long duration; // variable for the duration of sound wave travel
+    int distance;
+    int echoPin = 2; // attach pin D2 Arduino to pin Echo of HC-SR04
+    int trigPin = 3;
 };
 
 #endif
