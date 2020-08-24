c69af2b16b9aafa90ee9aa10dd139ae9ca35e0e7
Mathew Allington <funkymallington@gmail.com>
2020-08-24 21:26:13 +0100

    Testing sonic

--- host-firmware/src/sensors/SonicSensor.cpp
+++ host-firmware/src/sensors/SonicSensor.cpp
@@ -1,11 +1,31 @@
 #include <sensors/SonicSensor.h>
 
+
 SonicSensor::SonicSensor(String deviceIDPass):ITask(deviceIDPass){}
 bool SonicSensor::setup(DynamicJsonDocument* deviceJSON){
-    return false;
+     pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
+  Serial.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
+  Serial.println("with Arduino UNO R3");
+    return true;
 }
 DynamicJsonDocument* SonicSensor::getData(){
     DynamicJsonDocument jsonDocument(1024);
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(echoPin, HIGH);
+    // Calculating the distance
+    distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+    // Displays the distance on the Serial Monitor
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
     return &jsonDocument;
 }
 bool SonicSensor::updateState(DynamicJsonDocument* deviceJSON){
