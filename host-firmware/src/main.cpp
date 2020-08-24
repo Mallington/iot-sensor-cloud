d25daceb2ec66caaa29879a8ba96fe97bb5ce592
Mathew Allington <funkymallington@gmail.com>
2020-08-24 21:26:13 +0100

    Testing sonic

--- host-firmware/src/main.cpp
+++ host-firmware/src/main.cpp
@@ -1,51 +1,43 @@
-#include <scheduling/MainScheduler.h>
-#include <SPI.h>
-#include <WiFiNINA.h>
+#include <Arduino.h>
+// Sensor pins
+#define sensorPower 7
+#define sensorPin A0
 
-#include <network/WiFiUtils.h>
-#include "run_parameters.h"
-#include <network/RestAPI.h> 
-#include <sensors/SonicSensor.h>
-
-WiFiClient client;
-RestAPI api(API_ADDRESS, API_PORT, client);
+// Value for storing water level
+int val = 0;
+int avg =5;
+int avgs[5];
+int curr=0;
 void setup() {
-  //Initialize serial and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-
-  // check for the WiFi module:
-  int status = WiFiUtils.connectWiFI(WIFI_SSID, WIFI_PASS, 10000);
-  if(status == WL_CONNECTED){
-  Serial.println("Connected to wifi");
-  WiFiUtils.printWiFiStatus();
+	// Set D7 as an OUTPUT
+	pinMode(sensorPower, OUTPUT);
+	
+	// Set to LOW so no power flows through the sensor
+	digitalWrite(sensorPower, LOW);
+	
+	Serial.begin(9600);
 }
+
+//This is a function used to get the reading
+int readSensor() {
+	digitalWrite(sensorPower, HIGH);	// Turn the sensor ON
+	delay(10);							// wait 10 milliseconds
+	val = analogRead(sensorPin);		// Read the analog value form sensor
+	digitalWrite(sensorPower, LOW);		// Turn the sensor OFF
+	return val;							// send current reading
 }
 
 void loop() {
-    Serial.println("Connected");
-    String recieved = api.getRequest(String("/devices/")+DEVICE_ID, 2000, true);
-    Serial.println("R:"+recieved);
+	//get the reading from the function below and print it
+	int level = readSensor();
+  avgs[curr%avg] = level;
 
-    DynamicJsonDocument doc(1024);
-    DeserializationError error = deserializeJson(doc, recieved);
+  int fin =0;
 
-    if(error){
-      Serial.println("That didn't go well, couldn't parse the response!");
-      Serial.println(error.c_str());
-    }
-    const char* deviceName = doc["deviceName"];
-    Serial.println(deviceName); 
-
-    SonicSensor sensor("hello");
-    Serial.println(sensor.getDeviceID());
-
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting from server.");
-    client.stop();
-  }
-    while (Serial.available()<=0);
+  for(int i=0; i<avg; i++) fin+=avgs[i];
+	
+	
+	Serial.println(fin/avg);
+	curr+=1;
+	delay(100);
 }
