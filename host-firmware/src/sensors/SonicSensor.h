da3a0c38a65573a531bb5a3baa938de10a454f53
Mathew Allington <funkymallington@gmail.com>
2020-08-24 12:03:07 +0100

    Error with instantiating ITask
    collect2: error: ld returned 1 exit status
    *** [.pio/build/nano_33_iot/firmware.elf] Error 1

--- host-firmware/src/sensors/SonicSensor.h
+++ host-firmware/src/sensors/SonicSensor.h
@@ -4,9 +4,11 @@
 #include <scheduling/MainScheduler.h>
 
 class SonicSensor : public ITask{
-    virtual bool setup();
-    virtual DynamicJsonDocument& getData();
-    virtual bool updateState(DynamicJsonDocument& deviceJSON);
+public: 
+    SonicSensor(String deviceID);
+    virtual bool setup(DynamicJsonDocument* deviceJSON);
+    virtual DynamicJsonDocument* getData();
+    virtual bool updateState(DynamicJsonDocument* deviceJSON);
 };
 
 #endif
