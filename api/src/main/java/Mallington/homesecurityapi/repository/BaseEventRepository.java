2c659a8190ba13d0ddedffd2a8477bbe90203011
Mathew Allington <funkymallington@gmail.com>
2020-08-25 23:48:48 +0100

    Added /events endpoint
    Add endpoints that takes multiple sensor events inheriting BaseEvent class

--- api/src/main/java/Mallington/homesecurityapi/repository/BaseEventRepository.java
+++ api/src/main/java/Mallington/homesecurityapi/repository/BaseEventRepository.java
@@ -2,8 +2,12 @@ package Mallington.homesecurityapi.repository;
 
 import Mallington.homesecurityapi.model.BaseEvent;
 import Mallington.homesecurityapi.model.SonicEvent;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BaseEventRepository extends CrudRepository<BaseEvent, Integer> {
