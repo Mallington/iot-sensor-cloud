05c45bc1a2a88d1bc3f2938d7f9a99cc593c496e
Mathew Allington <funkymallington@gmail.com>
2020-08-26 13:50:35 +0100

    Scheduling is working a treat!

--- host-firmware/src/scheduling/MainScheduler.h
+++ host-firmware/src/scheduling/MainScheduler.h
@@ -5,6 +5,24 @@
 
 #define ARDUINOJSON_ENABLE_STD_STRING 0
 #define ARDUINOJSON_ENABLE_STD_STREAM 0
+#define MAIN_SCHEDULER_MAX_SIZE 1024
 #include <ArduinoJson.h>
+
+
+class MainScheduler{
+    public:
+        MainScheduler(long waitPeriod);
+        int getMaxCount();
+        long getWaitPeriod();
+        int count();
+        void tick();
+        void add(ITask* task);
+        void setup();
+    private:
+        int size =0;
+        long waitPeriod;
+        long last =0;
+        ITask* tasks[MAIN_SCHEDULER_MAX_SIZE];
+};
 
 #endif
