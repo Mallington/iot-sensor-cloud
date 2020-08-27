56e09e756e16d11dcb34b43217bfee876878085a
Mathew Allington <funkymallington@gmail.com>
2020-08-27 22:07:45 +0100

    Water depth widget style changes

--- frontend/src/components/widgets/WaterDepthSensorWidget.js
+++ frontend/src/components/widgets/WaterDepthSensorWidget.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import Chart from 'react-apexcharts'
+import ReactApexChart  from 'react-apexcharts'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import DeviceConfig from "../../configs/DeviceConfig";
 
 class WaterDepthSensorWidget extends Component {
 
@@ -13,7 +16,7 @@ class WaterDepthSensorWidget extends Component {
 
         this.state = {
             deviceID: props.deviceID,
-            options: {
+            lineChartOptions: {
                 chart: {
                     id: 'realtime',
                     height: 350,
@@ -36,9 +39,9 @@ class WaterDepthSensorWidget extends Component {
                     enabled: false
                 },
 
-                colors: ['#ffffff', '#66DA26', '#546E7A', '#E91E63', '#FF9800'],
+                colors: ['#f1f1f1', '#66DA26', '#5c7a54', '#E91E63', '#FF9800'],
                 stroke: {
-                    thickness: 4,
+                    thickness: 1,
                     curve: 'smooth'
                 },
                 markers: {
@@ -55,11 +58,53 @@ class WaterDepthSensorWidget extends Component {
                     show: false
                 },
             },
+            lineChartSeries: [{
+                data: []
+            }],
+            barChartOptions : {
+                colors: ['#f1f1f1', '#66DA26', '#5c7a54', '#E91E63', '#FF9800'],
+                chart: {
+                    type: 'bar',
+                    height: 20
+                },
+                plotOptions: {
+                    bar: {
+                        horizontal: true,
+                        dataLabels: {
+                            position: 'top',
+                        },
+                    }
+                },
+                dataLabels: {
+                    enabled: true,
+                    offsetX: -6,
+                    style: {
+                        fontSize: '12px',
+                        colors: ['#fff']
+                    }
+                },
+                stroke: {
+                    show: true,
+                    width: 1,
+                    colors: ['#fff']
+                },
+                xaxis: {
+                    categories: ["Level"],
+                    max:300
+                },
+                toolbar: {
+                    show: false
+                },
+                zoom: {
+                    enabled: false
+                },
+            },
             series: [{
                 data: []
             }],
-        };
-    }
+            };
+        }
+
 
     fetchData= () =>{
         console.log(this.currLength);
@@ -71,20 +116,25 @@ class WaterDepthSensorWidget extends Component {
         fetch('/events/filter?deviceId='+this.state.deviceID+"&idStart="+this.lastID+"&amount="+this.maxLength)
             .then(response => response.json())
             .then(data => {
-                console.log(data)
                 this.currLength = data.length;
                 this.latestID = data.pop().id;
-                this.setState({options: {
+                this.setState({lineChartOptions: {
                             xaxis: {
                                 categories: data.map((data)=>data.time)
                             }
                         },
-                        series: [{
+                    lineChartSeries: [{
                             data: data.map((data)=>data.depth)
-                        }]
+                        }],
+                    series:[{
+                        data: [data.pop().depth]
+                    }]
                     }
                 );
+                console.log(this.state.series)
             });
+
+
     }
 
     componentDidMount(){
@@ -103,19 +153,21 @@ class WaterDepthSensorWidget extends Component {
         clearInterval(this.timer)
     }
 
-    strip(attribute){
-        var data = [];
-        for(var a in this.exampleData){
-            data.push(a[attribute]);
-        }
-        return data;
-    }
-
     render() {
+        const mystyleBody = {
+            color: "white",
+            backgroundColor: "#000000",
+            width: 400,
+            fontFamily: "Roboto",
+            outline: "2px solid white",
 
+        };
         return (
-            <div className="line">
-                <Chart options={this.state.options} series={this.state.series} type="line" width="500"  />
+            <div className="line" style={mystyleBody} >
+                <h2 style={mystyleBody}>Water Sensor</h2>
+                <Chart options={this.state.lineChartOptions} series={this.state.lineChartSeries} type="line" width="400"  height={"200"}/>
+                <ReactApexChart  options={this.state.barChartOptions} type="bar" series={this.state.series} width="400" height="100"/>
+
             </div>
         );
     }
