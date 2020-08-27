ad5ef6b6b5e7c2cf354be522fa390be1ac13e610
Mathew Allington <funkymallington@gmail.com>
2020-08-27 13:28:14 +0100

    Removing credentials

--- host-firmware/src/run_parameters.h
+++ host-firmware/src/run_parameters.h
@@ -1,5 +1,5 @@
-#define WIFI_SSID "***REMOVED***"
-#define WIFI_PASS "***REMOVED***"
-#define API_ADDRESS "10.59.0.23"
-#define API_PORT 8080
-#define DEVICE_ID "8abb809773fedb7d0173fedb8ba60000"
+#define WIFI_SSID "$SSID_LOCATION"
+#define WIFI_PASS "$PASSWORD_LOCATION"
+#define API_ADDRESS "$API_ADDRESS"
+#define API_PORT $API_PORT
+#define DEVICE_ID "$DEVICE_ID"
