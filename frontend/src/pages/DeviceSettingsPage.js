16c8b3511d573618dd7d11571217bcc9044f7e75
Mathew Allington <funkymallington@gmail.com>
2020-10-04 14:59:38 +0100

    Device form start

--- frontend/src/pages/DeviceSettingsPage.js
+++ frontend/src/pages/DeviceSettingsPage.js
@@ -1,19 +1,41 @@
 import React from 'react'
-import DevicesCardCollection from "../components/DevicesCardCollection";
-import HostDevicesTable from "../components/HostDevicesTable"
+import Form from 'react-bootstrap/Form';
 export default class DeviceSettingsPage extends React.Component {
     render() {
         return (
 
             <div>
                 <h1>Device Settings</h1>
-                <form>
-                    <label>
-                        Name:
-                        <input type="text" name="name" />
-                    </label>
-                    <input type="submit" value="Submit" />
-                </form>
+                <Form>
+                    <Form.Group controlId="exampleForm.ControlInput1">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="name@example.com" />
+                    </Form.Group>
+                    <Form.Group controlId="exampleForm.ControlSelect1">
+                        <Form.Label>Example select</Form.Label>
+                        <Form.Control as="select">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="exampleForm.ControlSelect2">
+                        <Form.Label>Example multiple select</Form.Label>
+                        <Form.Control as="select" multiple>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Example textarea</Form.Label>
+                        <Form.Control as="textarea" rows="3" />
+                    </Form.Group>
+                </Form>
             </div>
         );
     }
