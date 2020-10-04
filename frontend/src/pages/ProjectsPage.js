c052728bf5eabc59672ba610bd1f56cbb1fc6d1b
Mathew Allington <funkymallington@gmail.com>
2020-10-04 23:44:05 +0100

    A good looking project page!

--- frontend/src/pages/ProjectsPage.js
+++ frontend/src/pages/ProjectsPage.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import DevicesCardCollection from "../components/DevicesCardCollection";
+import DeviceConfig from "../configs/DeviceConfig";
 export default class ProjectsPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            hosts: []
+        };
+
+    }
+    fetchData= () =>{
+        fetch('/devices?deviceType=HOST')
+            .then(response => response.json())
+            .then(devices => this.setState({hosts: devices}));
+    }
+
+    componentDidMount(){
+        this.fetchData();
+        this.timer = setInterval(this.fetchData, 5000);
+    }
+    componentWillUnmount() {
+        clearInterval(this.timer)
+    }
     render() {
         return (
 
             <div>
-                <DevicesCardCollection projectTitle="Nano V3.0 | Application Demo"/>
-                <DevicesCardCollection projectTitle="Cat Food and Water"/>
-                <DevicesCardCollection projectTitle="Weather Station"/>
+                {this.state.hosts.map((host)=><DevicesCardCollection key={host.id} host={host}/>)}
             </div>
         );
     }
