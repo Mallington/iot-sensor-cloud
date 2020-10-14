d7897f6c3a5f982b20630f9953dc65d352c5bfb3
Mathew Allington <funkymallington@gmail.com>
2020-10-14 15:46:31 +0100

    Firmware builder start

---
+++ firmware-builder/APIHelper.py
@@ -0,0 +1,28 @@
+import requests
+
+
+class APIHelper:
+
+    def __init__(self, api_endpoint, api_port):
+        self.apiEndpoint = api_endpoint
+        self.apiPort = api_port
+
+    @property
+    def apiPath(self):
+        return "http://{}:{}".format(self.apiEndpoint, self.apiPort)
+
+    def build_url(self, path):
+        return "{}{}".format(self.apiPath, path)
+
+    def connect(self):
+        try:
+            return requests.get(self.build_url("/devices?deviceType=HOST")).ok
+        except requests.exceptions.ConnectionError:
+            return False
+
+    def get_hosts(self):
+        return requests.get(self.build_url("/devices?deviceType=HOST")).content
+
+    def get_sensors(self, parent_id):
+        return requests.get(self.build_url("/devices?parentId={}".format(parent_id))).content
+
